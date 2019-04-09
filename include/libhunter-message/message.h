874375410f27f122ad08536fd1ca346680dd8381
Matt Woodward <matthew.woodward@swift-nav.com>
2019-04-09 13:26:56 +1000

    Initial commit

---
+++ include/libhunter-message/message.h
@@ -0,0 +1,6 @@
+#ifndef HUNTERMESSAGE_H
+#define HUNTERMESSAGE_H
+
+void hunter_message(const char *parent);
+
+#endif
---
+++ message.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include <libhunter-message/message.h>
+
+void hunter_message(const char *parent)
+{
+  printf("hunter_message v1 (%s)\n", parent ?: "(none)");
+}
